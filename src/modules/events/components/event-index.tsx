5cd85d799cbc749c5b1633a74454bedcdcbc6091
Loic Cordey <loic.cordey@gmail.com>
2026-07-12 12:45:33 +0200

    Refine event card menu behavior

--- src/modules/events/components/event-index.tsx
+++ src/modules/events/components/event-index.tsx
@@ -46,8 +46,10 @@ function EventCard({ canManage, event }: { canManage: boolean; event: EventSumma
       return;
     }
 
-    function handlePointerDown(event: MouseEvent) {
+    function handleDocumentClick(event: MouseEvent) {
       if (!menuRef.current?.contains(event.target as Node)) {
+        event.preventDefault();
+        event.stopPropagation();
         setIsMenuOpen(false);
       }
     }
@@ -58,11 +60,11 @@ function EventCard({ canManage, event }: { canManage: boolean; event: EventSumma
       }
     }
 
-    document.addEventListener("mousedown", handlePointerDown);
+    document.addEventListener("click", handleDocumentClick, true);
     document.addEventListener("keydown", handleEscape);
 
     return () => {
-      document.removeEventListener("mousedown", handlePointerDown);
+      document.removeEventListener("click", handleDocumentClick, true);
       document.removeEventListener("keydown", handleEscape);
     };
   }, [isMenuOpen]);
